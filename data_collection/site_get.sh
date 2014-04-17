4d228be2869e1bf32495689a1fb3793bc21d45ea
Luis Molina <luismol@umich.edu>
2014-04-16 22:12:12 -0400

    added  new pcap

--- data_collection/site_get.sh
+++ data_collection/site_get.sh
@@ -6,21 +6,21 @@ then
     echo "Using telex proxy @ 127.0.0.1:8888"
     for ((i=0; i < $1; i++ ))
     do
+        printf '%d of %d\n' $i $1
         phantomjs --disk-cache=false phantom_get.js $2
         phantomjs --disk-cache=false --proxy=127.0.0.1:8888 phantom_get.js $3
         #curl -silent -o curl_out.html $2
         #curl -silent -o curl_out.html $3
-        printf '%d of %d\n' $i $1
     done
 else
     echo "standard connection"
     for ((i=0; i < $1; i++ ))
     do
+        printf '%d of %d\n' $i $1
         phantomjs --disk-cache=false phantom_get.js $2
         phantomjs --disk-cache=false phantom_get.js $3
         #curl -silent -o curl_out.html $2
         #curl -silent -o curl_out.html $3
-        printf '%d of %d \r' $i $1
     done
 fi
 
