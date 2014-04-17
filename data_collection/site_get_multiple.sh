da0ad8b66e727f32e62465387d985a112f220f58
Nick Ruff <nickruff@umich.edu>
2014-04-16 20:55:36 -0400

    Site Get Multiple

---
+++ data_collection/site_get_multiple.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#Usage: sh site_get.sh <iterations> <site1> <site2> <site3> ...etc
+# each site should be followed by a y/n for Telex or No Telex
+# example: sh site_get.sh 100 google.com y baidu.com n something.org y yahoo.com n (etc)
+
+argz=("$@")
+maxargz="$#"
+
+iter=argz[0]
+
+i=1
+while (( i < maxargz ))
+do
+    site=${argz[$i]}
+    ((i += 1))
+    telex=${argz[$i]}
+    ((i += 1))
+    
+    for ((j=0; j<iter; j++))
+    do
+        if [ "$telex" = "y" ]
+        then
+            echo "Telex $site"
+            phantomjs --disk-cache=false --proxy=127.0.0.1:8888 phantom_get.js "$site"
+        else
+            echo "Nontelex $site"
+            phantomjs --disk-cache=false phantom_get.js "$site"
+        fi
+    done
+done
+
